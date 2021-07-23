5f1fc5bd3c2fa766941ae3e8eb4bd50a3d51c61c
BWolak3000 <77225663+BWolak3000@users.noreply.github.com>
2021-07-23 20:29:26 +0000

    Add files via upload

--- Witeboard/src/Main.py
+++ Witeboard/src/Main.py
@@ -1,20 +1,23 @@
 # Python 3.0
 # Bartosz Wolak
 # created 22.07.2021
-# updated 22.07.2021
+# updated 23.07.2021
 """
 Module responible for starting app and main loop
 """
 import os
-import pygame
 import src.WhiteBoardApp
+import pygame
 
 going = True
 
 
 def main():
     app = src.WhiteBoardApp.WhiteBoardApp()
+    clock = pygame.time.Clock()
+
     while going:
+        clock.tick(60)
         app.update()
 
 
---
+++ Witeboard/src/exit.py
@@ -0,0 +1,16 @@
+# Python 3.0
+# Bartosz Wolak
+# created 23.07.2021
+# updated 23.07.2021
+
+
+import pygame
+import os
+
+
+def quit_my_program():
+    pygame.quit()
+    exit(0)
+
+def nothing():
+    pass
